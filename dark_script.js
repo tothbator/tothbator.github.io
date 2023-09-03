db5dd3accfff46504f2092248e9d5f4908a512f5
tothbator <137621546+tothbator@users.noreply.github.com>
2023-09-03 15:51:18 +0200

    Update dark_script.js

--- dark_script.js
+++ dark_script.js
@@ -4,13 +4,13 @@
 var images = ['images/dark1.jpg', 'images/dark2.jpg', 'images/dark3.jpg', 'images/dark4.jpg',];
 var imageNumber = 0;
 
-function changeImage() {
+function changeImageDark() {
     imageNumber = (imageNumber + 1) % images.length;
     document.getElementById('image' + (imageNumber + 1)).src = images[imageNumber];
     hideImages(imageNumber);
 }
 
-function hideImages(currentImage) {
+function hideImagesDark(currentImageDark) {
     for (let i = 1; i <= images.length; i++) {
         let imageElement = document.getElementById('image' + i);
         imageElement.style.display = (i - 1 === currentImage) ? "block" : "none";
